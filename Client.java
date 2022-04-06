9a2cb0446358089ade8236b8412eaa8cb8f2dd10
Henry Wan <z5338828@ad.unsw.edu.au>
2022-04-06 22:58:40 +1000

    Development: Completed initial error checking

--- Client.java
+++ Client.java
@@ -3,6 +3,38 @@ import java.net.*;
 
 public class Client {
     public static void main(String args[]) throws Exception {
-        
+        // Exit the program if there are errors in args
+        if (hasErrorInit(args)) {
+            System.exit(1);
+        }
+
+        int portNum = getPortNum(args[0]);
+        System.out.println("Server port is " + portNum);
+    }
+
+    // Check if there is error
+    // i.e. Whether user has input args with lengh that is not 1
+    private static boolean hasErrorInit(String args[]) {
+        if (args.length != 1) {
+            System.out.println("Program only accept one argument!");
+            System.out.println("\nE.g.\njava Client <Server Port>");
+            return true;
+        }
+
+        return false;
+    }
+
+    // Gets the port number
+    // Exits if parsing is failed
+    private static int getPortNum(String portStr) {
+        // dummy value
+        int portNum = 0;
+        try {
+            portNum = Integer.parseInt(portStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Argument must be an integer");
+            System.exit(1);
+        }
+        return portNum;
     }
 }
